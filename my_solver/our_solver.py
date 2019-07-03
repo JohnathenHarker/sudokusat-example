70a0e506e4a039b86ded00480bc5240c04d361a6
Tobias John <tobiaserfurt@gmx.de>
2019-07-03 18:30:30 +0200

    implement output

--- my_solver/our_solver.py
+++ my_solver/our_solver.py
@@ -5,7 +5,7 @@ import math
 
 
 MAX_NUMBER = 0  # typically 9, 16, 25,...
-DIMENSION = 0
+DIMENSION = 0   # typically 3,  4,  5,...
 
 
 
@@ -14,13 +14,17 @@ def read_input(input_path):
     with open(input_path, 'r') as input_file:
         global MAX_NUMBER
         global DIMENSION
-        line = input_file.readline()            # do not need this line
-        line = input_file.readline()
-        _, number_of_tasks = line.split(': ')   # do not need this
-        line = input_file.readline()
-        _, task = line.split(': ')              # do not need this
+        global first_lines
+        first_lines = []
+        line = input_file.readline()    # do not need this line
+        first_lines.append(line)
+        line = input_file.readline()    # do not need this line
+        first_lines.append(line)
+        line = input_file.readline()    # do not need this line
+        first_lines.append(line)
 
         line = input_file.readline()
+        first_lines.append(line)
         _, size = line.split(': ') 
         MAX_NUMBER,_ = size.split('x')  # typically 9, 16, 25,...
         MAX_NUMBER = int(MAX_NUMBER)
@@ -36,9 +40,13 @@ def read_input(input_path):
                 numbers = numbers[1: len(numbers) -1]   # delete first and last element   
                 s = ""
                 for j in range(DIMENSION):
-                   s = s + numbers[j][0: len(numbers[j])-1]
-                s = s[1:]   # delete first symbol (blank)
-                numbers = s.split(' ')
+                   s = s + numbers[j][0: len(numbers[j])]
+                n = s.split(' ')
+
+                numbers = []
+                for j in range(len(n)):
+                    if n[j] != '':
+                        numbers.append(n[j])
 
                 if MAX_NUMBER != len(numbers):
                     print("ERROR: something went horribly wrong :(")
@@ -49,9 +57,25 @@ def read_input(input_path):
                 i += 1
         return puzzle
 
+# prints the solved puzzle
+def print_output(solution):
+    for i in range(len(first_lines)):
+        print(first_lines[i], end = '')
 
+    symbol_length = len(str(MAX_NUMBER)) # each numer should have this length
+    for i in range(DIMENSION):
+        print(DIMENSION*("+"+(DIMENSION*(symbol_length+1)+1)*"-")+"+")
+        for l in range(DIMENSION):
+            for j in range(DIMENSION):
+                print("| ", end = '')
+                for k in range(DIMENSION):
+                    number = solution[i*DIMENSION+l][j*DIMENSION+k]
+                    number = max(number,2)
+                    empty = (symbol_length-len(str(number)))*" "
+                    print(empty + str(solution[i*DIMENSION+l][j*DIMENSION+k]), end = ' ')
+            print("|")
 
-
+    print(DIMENSION*("+"+(DIMENSION*(symbol_length+1)+1)*"-")+"+")
 
 solver = sys.argv[1]
 input_path = sys.argv[2]
@@ -59,4 +83,5 @@ input_path = sys.argv[2]
 #print("input_path:", input_path)
 #print("solver:", solver)
 puzzle = read_input(input_path)
-print(puzzle)
+#print(puzzle)
+print_output(puzzle)
