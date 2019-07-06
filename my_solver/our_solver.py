3a89c005708586258e5b2c0050d1f2fa8005e7b3
Patrick Wienhöft <patrick.wienhoeft@mailbox.tu-dresden.de>
2019-07-06 22:09:58 +0200

    implement primitive encoding and solving

--- my_solver/our_solver.py
+++ my_solver/our_solver.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
+import subprocess
 import math
+import itertools
 
 
 MAX_NUMBER = 0  # typically 9, 16, 25,...
@@ -77,14 +79,121 @@ def print_output(solution):
 
     print(DIMENSION*("+"+(DIMENSION*(symbol_length+1)+1)*"-")+"+")
 
-def solve(puzzle, solver):
-    # decode and call the solver
-    solution = puzzle   # just for testing
-    return solution
+
+def create_cnf(puzzle, path_to_cnf):
+    size = len(puzzle)
+
+    with open(path_to_cnf, 'w') as f:
+
+        f.write("p cnf " + str(size**3) + " 12345\n")
+
+        # cell restrictions
+        for row in range(size):
+            for col in range(size):
+                cell_vars = []
+                for val in range(1, size+1):
+                    cell_vars.append(cell_to_int(row, col, val, size))
+                f.write(exactly_one_out_of(cell_vars))
+
+        # row restrictions
+        for col in range(size):
+            for val in range(1, size+1):
+                row_vars = []
+                for row in range(size):
+                    row_vars.append(cell_to_int(row, col, val, size))
+                f.write(exactly_one_out_of(row_vars))
+
+        # col restrictions
+        for row in range(size):
+            for val in range(1, size+1):
+                col_vars = []
+                for col in range(size):
+                    col_vars.append(cell_to_int(row, col, val, size))
+                f.write(exactly_one_out_of(col_vars))
+
+        # subsudoku restrictions
+        sub_size = round(math.sqrt(size))
+        for val in range(1, size + 1):
+            for subsudoku_row in range(sub_size):
+                for subsudoku_col in range(sub_size):
+                    subsudoku_vars = []
+                    for row in range(subsudoku_row*sub_size, (subsudoku_row+1)*sub_size):
+                        for col in range(subsudoku_col*sub_size, (subsudoku_col+1)*sub_size):
+                            subsudoku_vars.append(cell_to_int(row, col, val, size))
+                    f.write(exactly_one_out_of(subsudoku_vars))
+
+        # given variables
+        for row_index, row in enumerate(puzzle):
+            for col_index, cell in enumerate(row):
+                if cell != 0:
+                    f.write(str(cell_to_int(row_index, col_index, cell, size)) + " 0\n")
+
+
+def exactly_one_out_of(list_of_vars):
+    """
+    Return a CNF that ensures exactly one out of a given list of variables is true for all satisfying assignments
+    """
+    ret = ""
+    # at least one
+    ret += " ".join(str(i) for i in list_of_vars) + " 0\n"
+    # no two different variables
+    for pair in itertools.combinations(list_of_vars, 2):
+        ret += " ".join("-"+str(i) for i in pair) + " 0\n"
+
+    return ret
+
+
+def cell_to_int(row, col, val, size):
+    """
+    Return the integer that is the propositional variable representing
+    val being present int cell (row, col) in a sudoku of given size
+    """
+    # note val ranges from 1 to size, so 0 is impossible
+    return row * size**2 + col * size + val
+
+
+def int_to_cell(prop_var, size):
+    """
+    Return the row, column and value of a cell represented
+    by a propositional variable in a sudoku of given size
+    """
+    prop_var -= 1
+    row = prop_var // size**2
+    col = (prop_var % size**2) // size
+    val = prop_var % size
+    return row, col, val+1
+
+
+def solve(puzzle, solver, input_path):
+    size = len(puzzle)
+    cnf_path = input_path[:-3] + "cnf"
+    create_cnf(puzzle, cnf_path)
+
+    print("finished cnf")
+
+    if solver != "clasp":
+        print("Only supporting clasp atm")
+        return
+
+    clasp_out = subprocess.run(["clasp", "1", cnf_path], capture_output=True)
+
+    # process clasp output
+    # TODO make this linux-compatible (which it probably isn't rn)
+    for line in str(clasp_out.stdout).split("\\r\\n"):
+        if line[0] == "v":
+            variables = line[2:].split(" ")
+            for v in variables:
+                prop_var = int(v)
+                if prop_var > 0:
+                    row, col, cell = int_to_cell(prop_var, size)
+                    puzzle[row][col] = cell
+
+    return puzzle
+
 
 solver = sys.argv[1]
 input_path = sys.argv[2]
 
 puzzle = read_input(input_path)
-solution = solve(puzzle, solver)
+solution = solve(puzzle, solver, input_path)
 print_output(solution)
