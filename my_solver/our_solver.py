71b77a13f47c358ddee0f90cc328cdf8df1f02af
Patrick Wienhöft <patrick.wienhoeft@mailbox.tu-dresden.de>
2019-07-07 16:31:34 +0200

    add x wing preprocessing

--- my_solver/our_solver.py
+++ my_solver/our_solver.py
@@ -251,7 +251,7 @@ def preprocess(puzzle):
 
     changes = True
     iterations = 0
-    while changes and iterations < 5:
+    while changes and iterations < 10:
         changes = False
         iterations += 1
         numchanges = 0
@@ -386,6 +386,57 @@ def preprocess(puzzle):
                                 puzzle[r_del][candidate_col].remove(val)
                                 changes = True
                                 numchanges += 1
+
+        # Classic X wing
+        for val in range(1, size+1):
+            # with locked rows
+            locked_pairs = {}
+            for row_index in range(size):
+                occurrences = [col for col in range(size) if val in puzzle[row_index][col]]
+                if len(occurrences) == 2:
+                    if (occurrences[0], occurrences[1]) in locked_pairs.values():
+                        for r in locked_pairs:
+                            if locked_pairs[r] == (occurrences[0], occurrences[1]):
+                                other_row = r
+                                break
+                        for r in range(size):
+                            if r != row_index and r != other_row:
+                                if val in puzzle[r][occurrences[0]]:
+                                    puzzle[r][occurrences[0]].remove(val)
+                                    changes = True
+                                    numchanges += 1
+                                    print("did sth")
+                                if val in puzzle[r][occurrences[1]]:
+                                    puzzle[r][occurrences[1]].remove(val)
+                                    changes = True
+                                    numchanges += 1
+                                    print("did sth")
+                    locked_pairs[row_index] = (occurrences[0], occurrences[1])
+
+            # with locked cols
+            locked_pairs = {}
+            for col_index in range(size):
+                occurrences = [row for row in range(size) if val in puzzle[row][col_index]]
+                if len(occurrences) == 2:
+                    if (occurrences[0], occurrences[1]) in locked_pairs.values():
+                        for c in locked_pairs:
+                            if locked_pairs[c] == (occurrences[0], occurrences[1]):
+                                other_col = c
+                                break
+                        for c in range(size):
+                            if c != col_index and c != other_col:
+                                if val in puzzle[occurrences[0]][c]:
+                                    puzzle[occurrences[0]][c].remove(val)
+                                    changes = True
+                                    numchanges += 1
+                                    print("did sth")
+                                if val in puzzle[occurrences[1]][c]:
+                                    puzzle[occurrences[1]][c].remove(val)
+                                    changes = True
+                                    numchanges += 1
+                                    print("did sth")
+                    locked_pairs[col_index] = (occurrences[0], occurrences[1])
+
         print(numchanges)
 
 
